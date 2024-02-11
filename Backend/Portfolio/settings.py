78aef90f31728bfd71e80b9e5d053e319bb26202
Sanskargoyal608 <sanskargoyal608@gmail.com>
2024-02-11 21:07:33 +0530

    fsdf

--- Backend/Portfolio/settings.py
+++ Backend/Portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#$(607mpb-0=#77f&n#mnsxhq04+zi1xn-f=y658rue_)pc!1w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 PORT = 8000
 
 
